cd1d3c7612b4a1d55c2d7f5ba83a2c1d9be5a8e4
윤선용 <sunyong9059@naver.com>
2023-03-08 22:41:40 +0900

    카카오버튼 컴포넌트 생성

---
+++ src/components/login/KakaoBt.tsx
@@ -0,0 +1,11 @@
+import "../../css/intro.css"
+
+const KakaoBt = (props : any) => {
+  return (
+    <div className='kakaoBt'>
+        <h3>{props.name}</h3>
+    </div>
+  )
+}
+
+export default KakaoBt
